85fb5770d448728c5d1d0adc2e650f53a0babe95
Godwayne21 <g.rasalan@its-ictacademy.com>
2025-02-18 12:59:45 +0100

    Mario Cart

---
+++ Lezione_03/ES_Mario.py
@@ -0,0 +1,16 @@
+#Mario Cart
+
+n:int = (input("What's your position?: "))
+
+
+if n == 1:
+   print("You're 1st!")
+
+elif n == 2:
+    print("You're 2nd!")
+
+elif n == 3:
+    print ("You're 3rd")
+
+else:
+    print (f"You're {n}th! ")
