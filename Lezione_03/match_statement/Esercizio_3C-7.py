9ebfb210b3d94b354438a654f37280fed82438f1
Godwayne21 <g.rasalan@its-ictacademy.com>
2025-02-27 16:49:16 +0100

    match

---
+++ Lezione_03/match_statement/Esercizio_3C-7.py
@@ -0,0 +1,13 @@
+# Esercizio 3C-7
+'''
+Si scriva un programma in python che computi la statistica di otto lanci di una moneta.
+Per ciascuno dei lanci effettuati, l'utente inserisce "t" o "T"
+se è uscito "testa", mentre inserisce "c" o "C" se è uscito "croce".
+Il programma deve mostrare in output il numero totale e la percentuale dei risultati "testa" e "croce".
+NOTA.
+Le percentuali devono essere mostrate in output obbligatoriamente con 2 cifre decimali.
+Usare il match statement.
+'''
+lancio = ("Testa o Croce? ")
+volte = range(1,9)
+
