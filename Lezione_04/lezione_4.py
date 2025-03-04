fbabd6434b13d1710e861743a2c09ccd21de3850
Godwayne21 <g.rasalan@its-ictacademy.com>
2025-03-04 13:56:06 +0100

    funzioni

---
+++ Lezione_04/lezione_4.py
@@ -0,0 +1,28 @@
+#8-1. Message
+'''
+def display_message(a:str):
+
+    print (f"{a} is still learning about this chapter.")
+
+display_message("Godwayne")
+'''
+
+#8-2. Favorite Book
+'''
+def favorite_book(title):
+
+    print(f"One of my favorite books is {title}")
+
+favorite_book ("Dizionario")
+'''
+
+#8-3. T-Shirt
+'''
+def make_shirt(size,text:str):
+
+    print(f"The t-shirt size is {size} and the message on it is: {text}")
+
+make_shirt("M", "Aura")
+make_shirt (size = "S",text = "Wow")
+make_shirt (text = "Zoo",size = "L")
+'''
