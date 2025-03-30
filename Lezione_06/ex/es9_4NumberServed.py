df9a34290ffc74e80e394bc7a8c10edb05d82fb7
Godwayne21 <g.rasalan@its-ictacademy.com>
2025-03-30 12:28:54 +0200

    classi

--- Lezione_06/ex/es9_4NumberServed.py
+++ Lezione_06/ex/es9_4NumberServed.py
@@ -1,2 +1,21 @@
 # 9-4. Number Served
 
+class Restaurant:
+
+    def __init__(self, restaurant_name, cuisine_type) -> None:
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self) -> None:
+        print(f"The name is: {self.restaurant_name}\nThe cuisine is: {self.cuisine_type}")
+    
+    def open_restaurant(self) -> None:
+        print ("The restaurant is open!")
+    
+    def number_served(x=0) -> None:
+        print (x)
+        return x
+
+served = Restaurant
+
+served.number_served()
