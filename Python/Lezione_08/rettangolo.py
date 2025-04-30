6c51110002da533029bbe2989bc8148582a7f336
Godwayne21 <g.rasalan@its-ictacademy.com>
2025-04-30 13:47:55 +0200

    G

--- Python/Lezione_08/rettangolo.py
+++ Python/Lezione_08/rettangolo.py
@@ -9,3 +9,24 @@ class Rettangolo(FormaGenerica):
         self.setShape("rettangolo")
     
     def draw(self)->None:
+        print(f"\n {self.getShape()}\n")
+    
+        #outer for
+        for i in range(5):
+
+            #inner for
+            for j in range(5*2):
+
+                #lato a e lato d del rettangolo
+                if i == 0 or i == 5-1:
+                    print("*", end =" ")
+                
+                # lato c e lato b del rettangolo
+                elif j == 0 or j == (5*2)-1:
+                    print("*", end = " ")
+                
+                # stampare spazi bianchi
+                else:
+                    print(" ", end = " ")
+            
+            print ("\n", end = " ")
