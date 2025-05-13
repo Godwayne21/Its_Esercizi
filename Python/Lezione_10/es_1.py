fad7f365ad5981d89a7ee6247e48d689db42625f
Godwayne21 <g.rasalan@its-ictacademy.com>
2025-05-13 23:29:29 +0200

    rip

--- Python/Lezione_10/es_1.py
+++ Python/Lezione_10/es_1.py
@@ -2,11 +2,18 @@
 Scrivere il frammento di codice che cambi il valore intero memorizzato nella variabile x nel seguente modo:
 - se x è pari, deve essere diviso per 2;
 - se x è dispari deve essere moltiplicato per 3 e gli deve essere sottratto 1.
+Test case:
+
+print(transform(4)) #  Output: 2
+print(transform(-10)) #  Output: -5
 '''
 
-x: int = int(input("Dammi un numero: "))
+def transform(x:int):
 
-if x % 2 == 0:
-    print(x / 2)
-else:
-    print(x * 3 + 1)
+    if x % 2 == 0:
+        return x / 2
+    else:
+        return x * 3 + 1
+    
+print(transform(4)) #  Output: 2
+print(transform(-10)) #  Output: -5
---
+++ Python/Lezione_10/es_2.py
@@ -0,0 +1,21 @@
+'''
+Sviluppare una funzione in Python per calcolare lo stipendio lordo di ciascuno dei diversi impiegati. L'azienda paga 10 dollari all'ora per le prime 40 ore di lavoro e paga "una volta e mezza" la paga oraria per tutte le ore di lavoro oltre le 40 ore.
+ 
+Per ogni operaio, viene fornito il numero di ore che tale impiegato ha lavorato durante la settimana.
+La vostra funzione deve ricevere questa informazione per ogni impiegato e determinare e stampare lo stipendio lordo.
+
+Test case:
+
+print(calcola_stipendio(40)) # Output: 400.0
+'''
+
+def calcola_stipendio(x:int):
+
+    if x <= 40:
+        return x * 10
+    
+    else:
+        extra = x - 40
+        normale = x -extra
+
+        return extra * 15
