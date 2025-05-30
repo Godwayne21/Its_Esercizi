d0cfb2d6db653c73a18abaf3e616ae1965b2e5f1
Godwayne21 <g.rasalan@its-ictacademy.com>
2025-05-30 15:47:19 +0200

    es

--- Python/Lezione_10/es_1.py
+++ Python/Lezione_10/es_1.py
@@ -1,14 +1,13 @@
 '''
-Scrivere il frammento di codice che cambi il valore intero memorizzato nella variabile x nel seguente modo:
+1. Scrivere il frammento di codice che cambi il valore intero memorizzato nella variabile x nel seguente modo:
 - se x è pari, deve essere diviso per 2;
 - se x è dispari deve essere moltiplicato per 3 e gli deve essere sottratto 1.
 Test case:
-
 print(transform(4)) #  Output: 2
 print(transform(-10)) #  Output: -5
 '''
 
-def transform(x:int):
+def transform(x:int)->int:
 
     if x % 2 == 0:
         return x / 2
