bf720f2f74f95c0521e4f83e219934bffd418290
Godwayne21 <g.rasalan@its-ictacademy.com>
2025-06-12 10:21:10 +0200

    azienda

---
+++ Progettazione/Python/Azienda 1/azienda1_ristrutturato.py
@@ -0,0 +1,30 @@
+from enum import *
+from typing import Any
+from datetime import date,datetime
+
+class Impiegato:
+
+    def __init__(self, nome:str, cognome:str, nascita: date, stipendio:float)->None:
+
+        self.nome = nome
+        self.cognome = cognome
+        self.nascita = nascita
+        self.stipendio = stipendio
+
+    def get_nome (self, name:str)->str:
+        return self.nome
+    
+    def get_cognome (self, cognome:str)->str:
+        return self.cognome
+    
+    def get_nascita (self, nascita:date)->date:
+        return self.nascita
+    
+    def get_stipendio (self, stipendio:float)->str:
+        return self.stipendio
+
+    def __hash__(self)->int:
+        return (self.nome(),self.cognome(),self.nascita(),self.stipendio())
+    
+    def __eq__(self, other:Any)->bool:
+        
