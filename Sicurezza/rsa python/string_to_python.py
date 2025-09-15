a30648b4018c2096b5c615181638ce553202957a
Godwayne21 <g.rasalan@its-ictacademy.com>
2025-09-15 13:26:32 +0200

    rsa

---
+++ Sicurezza/rsa python/string_to_python.py
@@ -0,0 +1,26 @@
+m = 'Conversione'
+
+Mi=int(m.encode("utf-8").hex(), 16)
+
+print (Mi)
+
+n = "00c619524d066ad86300021e0172b5"+\
+    "830689fa913852237518a2eeee169a"+\
+    "cabbb552a4f97e1ed7b5afbdd9e2fa"+\
+    "723d5582489c983c0bdc7128a56b42"+\
+    "0cb45db16b8b0dd47f48ece19ccbcd"+\
+    "0eb847e6f01a77ca92825f2b89c35b"+\
+    "f2ac59c79c280658ba263b5ebb51e0"+\
+    "99413a1577bc8667a8af6c86d0096c"+\
+    "1de2050fe201be8f508e333de73210"+\
+    "e0abf06004a1d2fbfc40a44e2c2aff"+\
+    "638044bf89de8a638e62393d32bc71"+\
+    "83a3914fc299e5230221be952a8997"+\
+    "1a538ea3de8758f993a8b2adae2803"+\
+    "7a7cc546fb6627d82c6515782fa659"+\
+    "b37d434f4751078fa9bd5e9c055c7c"+\
+    "b17b4e05e5ef127b37803cefc986d4"+\
+    "a6a5ddf06d9c43dc97b37abb496845"+\
+    "3a5f"
+ 
+decimale = (n,16)
