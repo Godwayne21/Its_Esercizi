02b83951e3602b58c9b19c93748cf889066eaa03
Godwayne21 <g.rasalan@its-ictacademy.com>
2025-10-01 10:23:56 +0200

    todo

--- Web/secondaApp/src/App.jsx
+++ Web/secondaApp/src/App.jsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import UserCrud from './user-crud-analyst/UserCrud'
-import ToDoApp from '../ToDoApp'
+import ToDoApp from './ToDoApp'
+ToDoApp
 
 function App() {
 
--- Web/secondaApp/src/ToDoApp.jsx
+++ Web/secondaApp/src/ToDoApp.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import ToDoForm from './ToDoForm'
 import ToDoList from './ToDoList'
 import { useState, useEffect } from 'react'
+import { createTask, deleteTaskS, fetchTasks, toggleTaskS } from './api'
 
-const API_URL = "http://localhost:3000/tasks"
+
+const API_URL = "http://localhost:4000/tasks"
 const ToDoApp = () => {
     const [tasks, setTasks] = useState([])
     const getTasks = async () => {
         try {
             const response = await fetch(API_URL);
             if (!response.ok) throw new Error("Errore fetch :(")
-            const data = await response.json();
+            const data = await fetchTasks();
             console.log(data)
             setTasks(data)
         } catch (err) {
@@ -19,23 +21,28 @@ const ToDoApp = () => {
     };
 
     const addTask=async (text) =>{
-    await fetch(API_URL, {
+ /*   await fetch(API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body:JSON.stringify({text,completed:false})
-    });
+    });*/
+    await createTask(text);
     getTasks();
   }
     const deleteTask = async (id) => {
-        await fetch(API_URL + "/" + id, { method: "DELETE" });
+    //    await fetch(API_URL + "/" + id, { method: "DELETE" });
+
+        await deleteTaskS(id);
         getTasks();
     }
 
-    const toggleTask = async (id, completed) => {
-        await fetch(API_URL + "/" + id, {
+   const toggleTask = async (id, completed) => {
+/*        await fetch(API_URL + "/" + id, {
             method: "PATCH",
             headers: { "Content-Type": "application/json" }, body: JSON.stringify({ completed: !completed })
-        });
+        });*/
+
+        await toggleTaskS (id,completed);
         getTasks();
     }
     useEffect(() => {
