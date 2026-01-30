d92651da3c011361638b2e701ad310deaff48967
Godwayne21 <g.rasalan@its-ictacademy.com>
2026-01-30 11:29:38 +0100

    flags

--- Sicurezza/prova.py
+++ Sicurezza/prova.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python3
 
-from Crypto.Util.number import getPrime, inverse, bytes_to_long, long_to_bytes
-
+from Crypto.Util.number import getPrime, inverse, bytes_to_long, long_to_bytes, GCD
+#invertiamio il codice per decifrafe
+# n, e e ct sono stati forniti
+n = 984994081290620368062168960884976209711107645166770780785733
 e = 0x10001
+ct = 948553474947320504624302879933619818331484350431616834086273
+# n will be 8 * (100 + 100) = 1600 bits strong (I think?) which is pretty good
+#fattorizzo n 
+p = 848445505077945374527983649411
+q = 1160939713152385063689030212503
 
-p = getPrime(800)
-q = getPrime(800)
-n = p * q
+#calcolo rsa
 phi = (p - 1) * (q - 1)
 d = inverse(e, phi)
 
-# Messaggio
-FLAG = b"crypto{???????????????}"
-pt = bytes_to_long(FLAG)
+# si toglie perche sovvrascrive la flag prima che possa essere calcolato
+'''FLAG = b"crypto{???????????????}"
+pt = bytes_to_long(FLAG)'''
 
-# Cifratura
-ct = pow(pt, e, n)
-
-
-print(f"n = {n}")
+#non necessario perche devo solo decifrafe
+'''print(f"n = {n}")
 print(f"e = {e}")
-print(f"ct = {ct}")
-
+print(f"ct = {ct}")'''
 
 pt = pow(ct, d, n)
-
-# Convertiamo il numero di nuovo in stringa (bytes)
 decrypted = long_to_bytes(pt)
+#tolgo perchè fa il controllo solo se decrypted e e Flag dsono uguali durante la cifratura
+#assert decrypted == FLAG
 
-assert decrypted == FLAG
-
-print (FLAG)
+print (decrypted)
