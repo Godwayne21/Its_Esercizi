6c77ccfb65ba40341648acc413586be5ccfbc3b8
Godwayne21 <g.rasalan@its-ictacademy.com>
2026-01-30 19:10:20 +0100

    FLAG

--- Sicurezza/prova.py
+++ Sicurezza/prova.py
@@ -1,32 +1,21 @@
 #!/usr/bin/env python3
 
-from Crypto.Util.number import getPrime, inverse, bytes_to_long, long_to_bytes, GCD
-#invertiamio il codice per decifrafe
-# n, e e ct sono stati forniti
-n = 984994081290620368062168960884976209711107645166770780785733
-e = 0x10001
-ct = 948553474947320504624302879933619818331484350431616834086273
-# n will be 8 * (100 + 100) = 1600 bits strong (I think?) which is pretty good
-#fattorizzo n 
-p = 848445505077945374527983649411
-q = 1160939713152385063689030212503
+from Crypto.Util.number import bytes_to_long, getPrime
+from secret import messages
 
-#calcolo rsa
-phi = (p - 1) * (q - 1)
-d = inverse(e, phi)
 
-# si toglie perche sovvrascrive la flag prima che possa essere calcolato
-'''FLAG = b"crypto{???????????????}"
-pt = bytes_to_long(FLAG)'''
+def RSA_encrypt(message):
+    m = bytes_to_long(message)
+    p = getPrime(1024)
+    q = getPrime(1024)
+    N = p * q
+    e = 3
+    c = pow(m, e, N)
+    return N, e, c
 
-#non necessario perche devo solo decifrafe
-'''print(f"n = {n}")
-print(f"e = {e}")
-print(f"ct = {ct}")'''
 
-pt = pow(ct, d, n)
-decrypted = long_to_bytes(pt)
-#tolgo perchè fa il controllo solo se decrypted e e Flag dsono uguali durante la cifratura
-#assert decrypted == FLAG
-
-print (decrypted)
+for m in messages:
+    N, e, c = RSA_encrypt(m)
+    print(f"n = {N}")
+    print(f"e = {e}")
+    print(f"c = {c}")W
