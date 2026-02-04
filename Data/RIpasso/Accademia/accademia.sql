66470d13cf56bcb26c8c0a7049f5d4bc49dc29c4
Godwayne21 <g.rasalan@its-ictacademy.com>
2026-02-04 22:13:22 +0100

    accademia

--- Data/RIpasso/Accademia/accademia.sql
+++ Data/RIpasso/Accademia/accademia.sql
@@ -1,10 +1,11 @@
-create type Strutturato as enum ('Ricercatore', 'Professore Associato', 'Professore Ordinato');
+create type Strutturato as enum ('Ricercatore', 'Professore Associato', 'Professore Ordinario');
 create type LavoroProgetto as enum('Ricerca e Sviluppo', 'Dimostrazione', 'Management', 'Altro');
 create type LavoroNonProgettuale as enum ('Didattica', 'Ricerca', 'Missione', 'Incontro Dipartimentale', 'Incontro Accademico', 'Altro');
 create type CausaAssenza as enum ('Chiusura Universitaria', 'Maternita', 'Malattia');
+
 create domain PosInteger as int check(value>=0);
 create domain StringaM as varchar(100);
-create domain NumeroOre as int check(value>0 and value <8);
+create domain NumeroOre as int check(value>=0 and value <=8);
 create domain Denaro as real check(value>=0);
 
 create table Persona(
@@ -18,22 +19,21 @@ create table Persona(
 create table Progetto(
     id PosInteger primary key,
     nome StringaM not null unique,
-    inizio data not null,
-    fine data not null,
+    inizio date not null,
+    fine date not null,
     budget Denaro not null,
-
-    check(inizio<fine),
+    check(inizio<fine)
 );
 
 create table WP(
     progetto PosInteger,
     id PosInteger,
+    nome StringaM not null,
     primary key(progetto, id),
-    inizio data not null,
-    fine data not null,
-
+    inizio date not null,
+    fine date not null,
     check(inizio<fine),
-    foreign key progetto references Progetto(id)
+    foreign key (progetto) references Progetto(id)
 );
 
 create table AttivitaProgetto(
@@ -41,30 +41,27 @@ create table AttivitaProgetto(
     persona PosInteger not null,
     progetto PosInteger not null,
     wp PosInteger not null,
-    giorno data not null,
+    giorno date not null,
     tipo LavoroProgetto not null,
     oreDurata NumeroOre not null,
-
-    foreign key persona references Persona(id),
-    foreign key (progetto,wp) references WP(progetto,id)
+    foreign key (persona) references Persona(id),
+    foreign key (progetto, wp) references WP(progetto, id)
 );
 
 create table AttivitaNonProgettuale(
     id PosInteger primary key,
     persona PosInteger not null,
     tipo LavoroNonProgettuale not null,
-    giorno data not null,
+    giorno date not null,
     oreDurata NumeroOre not null,
-
-    foreign key persona references Persona(id)
+    foreign key (persona) references Persona(id)
 );
 
 create table Assenza(
     id PosInteger primary key,
-    persona PosInteger not null ,
+    persona PosInteger not null,
     tipo CausaAssenza not null,
-    giorno data not null ,
-
-    unique (persona,giorno),
-    foreign key persona references Persona(id)
+    giorno date not null,
+    unique (persona, giorno),
+    foreign key (persona) references Persona(id)
 );
